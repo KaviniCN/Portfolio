e20c90a5051745c0cedba7954983a941ca7624af
Shihan Anuruddha <shihan.anuruddha@activate.sg>
2023-06-30 23:43:21 +0800

    latest latest working

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,18 +7,24 @@ const Header = () => {
     <header className="">
       <nav className="text-cool-gray-100">
         <div className="flex font-sans font-normal text-base justify-center sm:justify-end mt-10 sm:gap-12 gap:6 sm:pr-20 mb-9 sm:mb-0">
-          {[
-            { label: "Work", route: "/" },
-            { label: "About", route: "/about" },
-            { label: "Resume", route: "/resume.pdf" },
-          ].map((item) => (
-            <Link to={item.route} className="mr-4 flex flex-col">
-              {item.label}
-              {item.route === location.pathname ? (
-                <div className="w-[10px] h-[10px] bg-[#8758FF] rounded-full m-auto" />
-              ) : null}
-            </Link>
-          ))}
+          <Link to="/">
+            Work
+            {location.pathname === "/" ? (
+              <div className="w-[10px] h-[10px] bg-[#8758FF] rounded-full m-auto" />
+            ) : null}
+          </Link>
+          <Link to="/about">
+            About
+            {location.pathname === "/about" ? (
+              <div className="w-[10px] h-[10px] bg-[#8758FF] rounded-full m-auto" />
+            ) : null}
+          </Link>
+          <a href="/resume.pdf" className="mr-4 flex flex-col">
+            Resume
+            {location.pathname === "/resume" ? (
+              <div className="w-[10px] h-[10px] bg-[#8758FF] rounded-full m-auto" />
+            ) : null}
+          </a>
         </div>
       </nav>
     </header>
--- src/index.js
+++ src/index.js
@@ -1,13 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import Home from "./pages/Home";
+import SFA from "./pages/SFA";
+import About from "./pages/About";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+// import Contact from "./Contact"
+// import Layout from "./components/Layout";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/about" element={<About />} />
+      <Route path="/case-study">
+        <Route path="sfa" element={<SFA />} />
+      </Route>
+    </>
+  )
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
